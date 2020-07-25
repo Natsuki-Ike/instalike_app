c5cdf083cd521939b81c70917c7dc92252dc8d45
Natsuki-Ike <ctppag@outlook.jp>
2020-07-25 14:17:07 +0900

    Add a Static Pages controller & add a test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Instalike App"
   end
 
+  test "should get lp" do
+    get static_pages_lp_url
+    assert_response :success
+    assert_select "title", "lp | Instalike App"
+  end
 end
