fe2b9e4e2101f49186083be727570c2a8b445758
Natsuki-Ike <ctppag@outlook.jp>
2020-07-25 14:45:38 +0900

    Refactoring title tag & static_pages view layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,15 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Instalike App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Instalike App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get lp" do
     get static_pages_lp_url
     assert_response :success
-    assert_select "title", "lp | Instalike App"
+    assert_select "title", "Lp | #{@base_title}"
   end
 end
